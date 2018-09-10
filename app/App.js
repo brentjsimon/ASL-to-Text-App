dad0435b88bf9bc146bd848fe9498a2fbe99d630
Kenny Leong <kenny.leong0221@yahoo.com>
2018-09-10 15:50:06 -0700

    mobile phone app

--- app/App.js
+++ app/App.js
@@ -1,56 +1,89 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View, Text, TouchableOpacity  } from 'react-native';
-import { AppLoading, Asset, Font, Icon, Camera, Permissions} from 'expo';
+import React, { Component } from 'react';
+import { AppRegistry, Platform, StatusBar, StyleSheet, View, Text, TouchableOpacity, Button, Alert } from 'react-native';
+import { AppLoading, Asset, Font, Icon, Camera, Permissions } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 
-export default class CameraExample extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    type: null,
-  };
-
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
-
-  render() {
-    const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
+export default class ProjectApp extends Component
+{
+    _onPressButton()
+    {
+        Alert.alert('You tapped the button!')
     }
-    else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: Camera.Constants.Type.back
-                  });
-                }}>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 20, color: 'white' }}>
-                  {' '}open camera{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
+    state =
+    {
+        hasCameraPermission: null,
+        type: null,
     }
-  }
+
+    async cameraMount()
+    {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState( { hasCameraPermission: status === 'granted' } );
+    }
+
+    render()
+    {
+        const { hasCameraPermission } = this.state;
+        if (hasCameraPermission === false)
+        {
+            return <Text> "No access to camera" </Text>;
+        }
+        else
+        {
+            return (
+              <Camera style = {{ flex: 1 }} 
+                      type = { this.state.type }
+                      ref = { ref => { this.camera = ref; }}>
+              <View style = { styles.container }>
+                <View style = { styles.buttonContainer }>
+                  <Button
+                onPress = { () => { this.setState({ type: Camera.Constants.Type.back }); } }
+                    title = "Back Camera"
+                    />
+                </View>
+                <View style = { styles.buttonContainer }>
+                    <Button
+                onPress = { () => { this.setState( { type: Camera.Constants.Type.front }); } }
+                    title = "Front Camera"
+                    color = "#841584"
+                    />
+                </View>
+                <View style = { styles.alternativeLayoutButtonContainer } >
+                  <Button
+                    onPress = { this._onPressButton } 
+                    title = "Pop-up"
+                    // color = "#(some number)" // to change color of text
+                  />
+                  <Button
+                    onPress = { this._onPressButton }
+                    title = "Pop-up2"
+                  />
+                </View>
+                </View>
+            </Camera>
+            );
+        }
+    }
 }
+
+const styles = StyleSheet.create(
+{
+    container: 
+    {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    buttonContainer:
+    {
+        margin: 20
+    },
+    alternativeLayoutButtonContainer:
+    {
+        margin: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+})
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => ButtonBasics);
