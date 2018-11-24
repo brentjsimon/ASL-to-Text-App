55c8e01d6e337c11c07e9ff70a337879bb6edcb6
Kenny Leong <kenny.leong0221@yahoo.com>
2018-11-23 18:38:52 -0800

    updated buttons to do run python print

---
+++ bootstrap-project/app/hello.py
@@ -0,0 +1,5 @@
+def hello_test():
+	print('hello world from ajax\n')
+
+def test():
+	print('this is an ajax call\n')
--- bootstrap-project/server.py
+++ bootstrap-project/server.py
@@ -1,16 +1,30 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template   
+import app.hello as hello
 
 app = Flask(__name__)
-app.debug = True
 
-
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET','POST'])
+# renders the view
 def index():
     if request.method == "POST":
         name = request.form["name"]
         return name + " Hello"
     return render_template("index.html")
 
+@app.route("/hello", methods=['GET'])
+def test():
+    hello.hello_test() # this prints out on to command prompt
+    return render_template("index.html")
+
+@app.route("/translate", methods=['GET'])
+def test2():
+    hello.test() # this prints out on to command prompt
+    return render_template("index.html")
+
+@app.route("/get_histo", methods=['GET'])
+def test3():
+    hello.test() # this prints out on to command prompt
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
