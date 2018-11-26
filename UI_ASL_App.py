efef9d0289385d836bb41725658418a07e5b9ce8
andrea-regina-sanchez <sanchez.andie@gmail.com>
2018-11-25 20:01:59 -0800

    Add files via upload

--- UI_ASL_App.py
+++ UI_ASL_App.py
@@ -1,10 +1,11 @@
-import sys, cv2, numpy, os, pickle, copy
+import sys, cv2, numpy, os, pickle, copy, sqlite3
 from new_gesture_app import add_gest
 from PyQt5.QtCore import QTimer, QUrl, QFileInfo, Qt
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog
 from PyQt5.uic import loadUi
+from keras.models import load_model
 
 #referenced webcam tutorial from Life2Coding: https://www.youtube.com/watch?v=MUpC6z32bCA
 
@@ -18,10 +19,12 @@ class aslApp(QDialog):
     vid_frm = None
     vid_open = False
     vid_orien = 0
+    trans_state = False
+    prev_text = ""
 
     def __init__(self):
         super(aslApp, self).__init__()
-        loadUi('UI_ASL_tab1.ui',self)
+        loadUi('UI_ASL_tab2.ui',self)
         self.image = None
         self.tabWidget.setCurrentIndex(0)
         current_tab = 0
@@ -39,9 +42,14 @@ class aslApp(QDialog):
         self.rotCCWBtn.clicked.connect(self.rot_vid_ccw)
         self.rotCWBtn.clicked.connect(self.rot_vid_cw)
         self.tabWidget.currentChanged.connect(self.change_webcam)
+        self.stopRadBtn.setChecked(True)
+        self.clearBtn.clicked.connect(self.clear_output)
+        #self.stopRadBtn.clicked.connect(lambda:self.radbtn_state(self.stopRadBtn))
+        #self.startRadBtn.clicked.connect(lambda:self.radbtn_state(self.startRadBtn))
+        self.get_image_size()
         self.start_webcam()
         self.tabWidget.blockSignals(False)
-    
+
     def change_webcam(self):
         if self.current_tab == 0:
             self.current_tab = 1
@@ -65,10 +73,10 @@ class aslApp(QDialog):
 
     def update_frame(self):
         ret,self.image=self.capture.read()
-        x, y, w, h = 300, 100, 300, 300
         self.image = cv2.flip(self.image,1)
-        
-        image_sample = self.image[y:y+h, x:x+w]
+
+        x, y, w, h = 300, 100, 300, 300
+        self.image_sample = self.image[y:y+h, x:x+w]
 
         display_img = copy.deepcopy(self.image)
 
@@ -100,7 +108,15 @@ class aslApp(QDialog):
         outImage = outImage.rgbSwapped()
         self.threshDisplay.setPixmap(QPixmap.fromImage(outImage))
         self.threshDisplay.setScaledContents(True)
-    
+        
+        if self.startRadBtn.isChecked():
+            self.translate(thresh)
+       # else:
+           # print("S T O P")
+
+   
+
+
     def capture_hist(self):
         #everytime you capture skin color, save to pickle bytestream file
         hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
@@ -117,15 +133,15 @@ class aslApp(QDialog):
                 self.hist = pickle.load(file)
     
     def calc_thresh(self):
-        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(self.image_sample, cv2.COLOR_BGR2HSV)
         dst = cv2.calcBackProject([hsv], [0, 1], self.hist, [0, 180, 0, 256], 1)
-        dst1 = dst.copy()
         disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
         cv2.filter2D(dst,-1,disc,dst)
         blur = cv2.GaussianBlur(dst, (11,11), 0)
         blur = cv2.medianBlur(blur, 15)
         ret,trsh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         trsh = cv2.merge((trsh,trsh,trsh))
+        #trsh = cv2.cvtColor(trsh, cv2.COLOR_BGR2GRAY)
         return trsh
 
     def build_squares(self,img):
@@ -150,6 +166,8 @@ class aslApp(QDialog):
             x = 420
             y+=h+d
         return crop
+
+        
 
     def upload_image(self):
         gest_img_file, _ = QFileDialog.getOpenFileName(self, 'Open file', '.',"Image files (*.jpg *.png)")
@@ -218,7 +236,91 @@ class aslApp(QDialog):
             new_gest_app.start()
         else:
             self.gestOutTxt.insertPlainText("Error: must fill in all fields.\n")
+
+    image_x = 0
+    image_y = 0    
+
+    def get_image_size(self):
+        img = cv2.imread('gestures/0/100.jpg', 0)
+        #return img.shape
+        self.image_x,self.image_y = img.shape
+
+    model = load_model('cnn_model_keras2.h5')
+
+    def keras_process_image(self, img):
+        img = cv2.resize(img, (self.image_x, self.image_y))
+        img = numpy.array(img, dtype=numpy.float32)                     ###
+        img = numpy.reshape(img, (1, self.image_x, self.image_y, 1))
+        return img
+
+    def keras_predict(self, model, image):
+        processed = self.keras_process_image(image)
+        pred_probab = model.predict(processed)[0]
+        pred_class = list(pred_probab).index(max(pred_probab))
+        return max(pred_probab), pred_class
+
+    def get_pred_text_from_db(self, pred_class):
+        conn = sqlite3.connect("gesture_db.db")
+        cmd = "SELECT g_name FROM gesture WHERE g_id="+str(pred_class)
+        cursor = conn.execute(cmd)
+        for row in cursor:
+            return row[0]
+
+    def split_sentence(self, text, num_of_words):
+        '''
+        Splits a text into group of num_of_words
+        '''
+        list_words = text.split(" ")
+        length = len(list_words)
+        splitted_sentence = []
+        b_index = 0
+        e_index = num_of_words
+        while length > 0:
+            part = ""
+            for word in list_words[b_index:e_index]:
+                part = part + " " + word
+            splitted_sentence.append(part)
+            b_index += num_of_words
+            e_index += num_of_words
+            length -= num_of_words
+        return splitted_sentence
+
+    def translate(self, trsh):
+        #print("translating...")
+
+        tresh = trsh.copy()
+        thresh = cv2.cvtColor(tresh, cv2.COLOR_BGR2GRAY)
+        '''thresh.copy()'''
+        contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+        if len(contours) > 0:
+            contour = max(contours, key = cv2.contourArea)
+            #print(cv2.contourArea(contour))
+            if cv2.contourArea(contour) > 10000:
+                x1, y1, w1, h1 = cv2.boundingRect(contour)
+                save_img = thresh[y1:y1+h1, x1:x1+w1]
+
+                if w1 > h1:
+                    save_img = cv2.copyMakeBorder(save_img, int((w1-h1)/2) , int((w1-h1)/2) , 0, 0, cv2.BORDER_CONSTANT, (0, 0, 0))
+                elif h1 > w1:
+                    save_img = cv2.copyMakeBorder(save_img, 0, 0, int((h1-w1)/2) , int((h1-w1)/2) , cv2.BORDER_CONSTANT, (0, 0, 0))
+
+                pred_probab, pred_class = self.keras_predict(self.model, thresh) #save_img)
+                #print(pred_class, pred_probab)
+
+                if pred_probab*100 > 80:
+                    text = self.get_pred_text_from_db(pred_class)
+                    #print(text)
+                    #splitted_text = self.split_sentence(text, 2)
+                    #str = ''.join(splitted_text)
+                    if text != self.prev_text:
+                        self.transOutTxt.insertPlainText(text)
+                        self.transOutTxt.insertPlainText(" ")
+                        self.prev_text = text
         
+        #find a way to not print the letter if the same as the previous letter
+    def clear_output(self):
+        self.transOutTxt.clear()
+  
 if __name__=='__main__':
     app = QApplication(sys.argv)
     window = aslApp()
